fb05aa76f35aa6c3529cce9641fd04e3f6ba6769
Georgi Lyubenov <godzbanebane@gmail.com>
2020-01-12 14:01:20 +0200

    Destroy delete obliterate index generating fns

--- minconflicts/src/MinConflicts.hs
+++ minconflicts/src/MinConflicts.hs
@@ -8,10 +8,13 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TupleSections #-}
 
-module MinConflicts where
+module MinConflicts
+  ( solveFor
+  , prettyState
+  ) where
 
 import Control.Monad (replicateM)
-import Data.List (intercalate, delete)
+import Data.List (intercalate)
 import Data.List.Extra (minimumOn)
 import Prelude hiding (curry)
 import Data.Vector (Vector, (!))
@@ -60,79 +63,6 @@ updateConflicts :: Location -> Location -> Conflicts -> Conflicts
 updateConflicts oldloc newloc
   = adjust (+ (-1)) oldloc
   . adjust (+1) newloc
-
--- TODO: why are the diag filters necessary
--- I thought I removed them in the diag generation..
-updateStraightConflicts :: Int -> Location -> Int -> [([Location], Int -> Int)]
-updateStraightConflicts n (x, oldy) newy =
-  let oldCol = filter (\loc -> not $ diag loc (x, newy)) $ genCol x n oldy
-      newCol = filter (\loc -> not $ diag loc (x, oldy)) $ genCol x n newy
-   in [(newCol, (+1)), (oldCol, (+ (-1)))]
-
-genRow :: Int -> Int -> a -> [(a, Int)]
-genRow col n x = map (x,) $ fromToExcept 0 n col
-{-# INLINE genRow #-}
-
-genCol :: Int -> Int -> a -> [(Int, a)]
-genCol row n y = map (,y) $ fromToExcept 0 n row
-{-# INLINE genCol #-}
-
-diag :: Location -> Location -> Bool
-diag (x, y) (x', y')
-  = (x, y) /= (x', y')
- && (abs (x - x') == abs (y - y'))
-{-# INLINE diag #-}
-
-revDiag :: (Int, Int) -> Int -> [(Int, Int)]
-revDiag (x', y') n = [(z, m - z) | m <- [x' + y'], z <- [0..m], z <= n, m - z <= n, z /= x']
-
-revDiag' :: (Int, Int) -> Int -> Int -> [(Int, Int)]
-revDiag' (x', y') n excy = [(z, m - z) | m <- [x' + y'], z <- [0..m], z <= n, m - z <= n, z /= x', (m - z) /= excy]
-
--- TODO: move the i /= * stuff to delete, probably
-mainDiag :: (Int, Int) -> Int -> [(Int, Int)]
-mainDiag (x', y') n =
-  let d = abs (x' - y')
-   in case compare x' y' of
-        EQ -> [(i, i) | i <- [0..n], i /= x']
-        LT -> [(i, i + d) | i <- [0..n - d], i /= x']
-        GT -> [(i + d, i) | i <- [0..n - d], i /= y']
-
--- TODO: move the excy stuff to delete, probably
--- excy is used to exclude crossing points with columns
-mainDiag' :: (Int, Int) -> Int -> Int -> [(Int, Int)]
-mainDiag' (x', y') n excy =
-  let d = abs (x' - y')
-   in case compare x' y' of
-        EQ -> [(i, i) | i <- [0..n], i /= x', i /= excy]
-        LT -> [(i, i + d) | i <- [0..n - d], i /= x', (i + d) /= excy]
-        GT -> [(i + d, i) | i <- [0..n - d], i /= y', i /= excy]
-
-updateDiagConflicts :: Int -> Location -> Int -> [([Location], Int -> Int)]
-updateDiagConflicts n (x, oldy) newy =
-  let oldRevDiag = revDiag' (x, oldy) n newy
-      newRevDiag = revDiag' (x, newy) n oldy
-
-      oldMainDiag = mainDiag' (x, oldy) n newy
-      newMainDiag = mainDiag' (x, newy) n oldy
-
-      oldDiag = oldRevDiag ++ oldMainDiag
-      newDiag = newRevDiag ++ newMainDiag
-
-      delCrossing xs =
-        let dy = abs $ oldy - newy
-            dist = div dy 2
-            st = min oldy newy
-         in
-          if even dy
-          then delete (x - dist, st + dist) $ delete (x + dist, st + dist) xs
-          else xs
-
-      oldDiag' = delCrossing oldDiag
-
-      newDiag' = delCrossing newDiag
-
-   in [(newDiag', (+1)), (oldDiag', (+ (-1)))]
 
 randomConflicting
   :: MonadState BoardState m
@@ -196,15 +126,6 @@ fillRandom n = do
         = foldr (adjust succ) emptyConflicts $ Vec.indexed board
 
   pure $ BoardState{..}
-
-placeConflicts :: Int -> Location -> [([Location], Int)]
-placeConflicts n loc = placeStraightConflicts n loc ++ placeDiagConflicts n loc
-
-placeStraightConflicts :: Int -> Location -> [([Location], Int)]
-placeStraightConflicts n (x, y) = [(genRow y n x, 1), (genCol x n y, 1)]
-
-placeDiagConflicts :: Int -> Location -> [([Location], Int)]
-placeDiagConflicts n loc = [(mainDiag loc n, 1), (revDiag loc n, 1)]
 
 printAsBoard :: Board -> String
 printAsBoard v = concat $ flip Vec.imap v \_ y -> rowWithMarked (length v - 1) y ++ "\n"
